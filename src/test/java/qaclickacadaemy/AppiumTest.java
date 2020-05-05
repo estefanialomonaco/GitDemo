d76953ace7b4569ffdab74a4fc8d135cadcbb6cc
stefi <estefanialomonaco@gmail.com>
2020-05-05 19:39:07 -0300

    first commit

---
+++ src/test/java/qaclickacadaemy/AppiumTest.java
@@ -0,0 +1,21 @@
+package qaclickacadaemy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	
+	@Test
+	public void NativeAPAndroid()
+	{
+	System.out.println("NativeAPAndroid");
+}
+
+	@Test
+	public void IOS()
+	
+	{
+		System.out.println("IOS");
+	}
+	
+}
---
+++ src/test/java/qaclickacadaemy/RESTAPITest.java
@@ -0,0 +1,26 @@
+package qaclickacadaemy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	
+	
+	@Test
+	public void posJira()
+	{
+		
+		System.out.println("POSjira");
+
+	}
+	
+	@Test
+	public void deleteTwitter(
+			) 
+	
+	{
+		
+		System.out.println("deleteTwitter");
+	}
+
+}
